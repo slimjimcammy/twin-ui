a1181fba99f7acfd5a5b3eb7749a52b9e3888082
Cameron Yuen <yuen.cameron@gmail.com>
2025-06-10 21:46:25 -0700

    index.html

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -1,45 +1,84 @@
-import { type ReactNode } from 'react';
-import { cva, type VariantProps } from 'class-variance-authority';
+import { cn } from "../cn";
+import { cva, type VariantProps } from "class-variance-authority";
 
-const containerVariants = cva(
-  // Base styles
-  'mx-auto w-full',
-  {
-    variants: {
-      width: {
-        full: 'max-w-none',
-        default: 'max-w-7xl',
-        narrow: 'max-w-5xl',
-        tight: 'max-w-3xl',
-      },
-      padding: {
-        none: 'px-0',
-        small: 'px-4',
-        default: 'px-4 sm:px-6 lg:px-8',
-        large: 'px-6 sm:px-8 lg:px-12',
-      },
+const containerVariants = cva("mx-auto w-full box-border", {
+  variants: {
+    width: {
+      full: "w-full",
+      default: "w-auto",
+      screen: "w-screen",
     },
-    defaultVariants: {
-      width: 'default',
-      padding: 'default',
+    height: {
+      full: "h-full",
+      default: "h-auto",
+      screen: "h-screen",
     },
-  }
-);
+    padding: {
+      none: "p-0",
+      small: "p-4",
+      default: "p-6",
+      large: "p-8",
+    },
+    border: {
+      none: "border-none",
+      all: "border-black border-2 border-solid",
+      top: "border-t-black border-t-2 border-t-solid",
+      bottom: "border-b-black border-b-2 border-b-solid",
+      left: "border-l-black border-l-2 border-l-solid",
+      right: "border-r-black border-r-2 border-r-solid",
+      topLeft: "border-tl-black border-tl-2 border-tl-solid",
+      topRight: "border-tr-black border-tr-2 border-tr-solid",
+      bottomLeft: "border-bl-black border-bl-2 border-bl-solid",
+      bottomRight: "border-br-black border-br-2 border-br-solid",
+      topBottom:
+        "border-t-black border-t-2 border-t-solid border-b-black border-b-2 border-b-solid",
+      leftRight:
+        "border-l-black border-l-2 border-l-solid border-r-black border-r-2 border-r-solid",
+      topLeftBottomRight:
+        "border-tl-black border-tl-2 border-tl-solid border-br-black border-br-2 border-br-solid",
+      topRightBottomLeft:
+        "border-tr-black border-tr-2 border-tr-solid border-bl-black border-bl-2 border-bl-solid",
+      topLeftBottom:
+        "border-tl-black border-tl-2 border-tl-solid border-b-black border-b-2 border-b-solid",
+      topRightBottom:
+        "border-tr-black border-tr-2 border-tr-solid border-b-black border-b-2 border-b-solid",
+      topLeftRight:
+        "border-tl-black border-tl-2 border-tl-solid border-r-black border-r-2 border-r-solid",
+      topRightLeft:
+        "border-tr-black border-tr-2 border-tr-solid border-l-black border-l-2 border-l-solid",
+      bottomLeftRight:
+        "border-bl-black border-bl-2 border-bl-solid border-r-black border-r-2 border-r-solid",
+      bottomRightLeft:
+        "border-br-black border-br-2 border-br-solid border-l-black border-l-2 border-l-solid",
+    },
+  },
+  defaultVariants: {
+    width: "default",
+    padding: "default",
+  },
+});
 
 export interface ContainerProps extends VariantProps<typeof containerVariants> {
-  children: ReactNode;
+  children: React.ReactNode;
   className?: string;
 }
 
 export function Container({
   children,
   width,
+  height,
   padding,
-  className = '',
+  className = "",
+  border,
 }: ContainerProps) {
   return (
-    <div className={containerVariants({ width, padding, className })}>
+    <div
+      className={cn(
+        containerVariants({ width, height, padding, border }),
+        className
+      )}
+    >
       {children}
     </div>
   );
-} 
+}
